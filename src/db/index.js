68e9eb1ce21152061c5502fd253df83fc47bbb29
Matheus <matheuses18@icloud.com>
2024-01-29 11:14:00 -0300

    update

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,10 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
-const db = new Pool({
+const db = Pool({
   host: PGHOST,
   database: PGDATABASE,
   user: PGUSER,
